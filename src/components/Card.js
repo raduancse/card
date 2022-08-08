9711de6eed66bd977ca7ced71e543a17eb36bdad
Md. Raduan Islam <raduan.cse@gmail.com>
2022-08-08 23:15:01 +0600

    card project live

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import ieb from "./ieb.png"
+function Card() {
+  return (
+    <div className="card">
+      <div className="header-content">
+        <div>{/* <img src={require("./ieb.png")} /> */}</div>
+        <div className="bio">
+          <h2 style={{color:'#996666'}}>Engr. Md. Raduan Islam</h2>
+          <p>Software Engineer, Large IT Solution, Dhaka</p>
+          <p>Associate Member, Institution of Engineers, Bangladesh (IEB), Dhaka</p>
+          <p style={{color:'green'}}>IEB Membership No: A-20089</p>
+        </div>
+      </div>
+      <div className="footer-content">
+        <p>Mobile: +8801675386216</p>
+        <p>E-mail: raduan.cse@gmail.com</p>
+        <p>Web: https://github.com/raduancse</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
